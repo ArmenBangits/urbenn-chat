87328ffa2cbc7c83c6d7bc0c81dcdbcd95acd537
ArmenMesropyan <armenbangits@gmail.com>
2021-02-23 14:10:24 +0400

    Some style changes

--- example/src/index.tsx
+++ example/src/index.tsx
@@ -1,5 +1,7 @@
+import 'bootstrap/scss/bootstrap.scss'
 import React from 'react'
 import ReactDOM from 'react-dom'
+import 'ti-icons/css/themify-icons.css'
 import App from './App'
 import './index.css'
 
